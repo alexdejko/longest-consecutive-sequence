c254cac93c92a2e11483cee44de5b50de5875d73
alexdejko <dag@brest.beltelecom.by>
2018-10-05 09:26:58 +0300

    Max Seq

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  
+ let arr= array.sort(function(a, b) {
+            return a - b;
+        });
+ 
+ if(array.length == 0){
+        return 0;
+    }else if(array.length == 1){
+        return 1;
+    }
+ function seq(arr){ 
+  let tmp = 1;
+  let max = 0;
+  for (let i = arr.length; i > 0; i--){
+    if (arr[i] == arr[i - 1] + 1) {
+       tmp=tmp + 1;
+       if(tmp > max){
+        max = tmp;
+       }
+    }else{tmp = 1}
+  }
+ return max;
+} 
+
+ return seq(array);
 }
+
